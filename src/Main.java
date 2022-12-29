f864631a763d5b028377c80f7c300f9ceaac534d
GIDRO32 <pambossable@gmail.com>
2022-12-29 12:30:02 +0200

    Add project

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+import java.util.Scanner;
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        int Money = 500;
+        if(Money > 2000)
+        {
+            System.out.println("Wow, this is plenty!");
+            if(Money != 2375)
+            {
+                System.out.println("But it isn't equal to my balance. You must guess it to win!");
+            }
+            else {
+                System.out.println("And you guessed my balance! Now my balance is yours!\nIsn't it great to have twice more than you had before?");
+            }
+        }
+        else {
+            System.out.println("You are poorer than me. I have nothing to talk about with you.");
+        }
     }
-}
+    }
